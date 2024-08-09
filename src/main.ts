c7567a5d205ba2768f69edd54a75700c6eec4b71
Bob He <bobhe@BobdeiMac.local>
2024-08-09 13:17:07 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,35 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+let app: any | null = null;
+function render(props: any = {}) {
+    const { container } = props;
+    app = createApp(App);
+    app.use(router)
+    app.use(store)
+    app.mount(container ? container.querySelector('#JClub') : '#JClub');
+}
+
+// 独立运行时
+if (!window.__POWERED_BY_QIANKUN__) {
+    render({});
+}
+
+export async function bootstrap() {
+    console.log('Vue3 app bootstraped');
+}
+
+export async function mount(props: any) {
+    console.log('Vue3 app mounted', props);
+    render(props);
+}
+
+export async function unmount() {
+    console.log('Vue3 app unmounted');
+    app?.unmount();
+    app = null;
+}
+
+export async function update(props: any) {
+    console.log('Vue3 app updated', props);
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const alias = {
+  '@': joinUrl('./src')
+}
+
+function joinUrl(src) {
+  return resolve(__dirname, src)
+}
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: { alias },
+    output: {
+      library: `club`,
+      libraryTarget: 'umd',
+    }
+  },
+  devServer: {
+    port: 8888,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
 })
